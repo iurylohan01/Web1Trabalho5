0407ad479376b1edb26df833d6155933c8fee4f4
Iury L. Murdock <iurylohan8@gmail.com>
2022-12-08 02:13:38 +0000

    Algumas mudanças

--- j.script/script.js
+++ j.script/script.js
@@ -1,37 +1,33 @@
-var msg1 = document.getElementById("m1");
-var msg2 = document.getElementById("m2");
-var msg3 = document.getElementById("m3");
-
-var resposta = Math.floor(Math.random()*30) + 1;
-var quant_resp = 0;
-var num_digit = [];
-
-function play(){
-    var user_resp = document.getElementById("resposta").value;
-    if(user_resp < 1 || user_resp > 30){
-        alert("Entre com um número de 1 a 30.");
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#nova_tarefa input').value.length == 0){
+        alert("Adicione uma tarefa primeiro.")
     }
     else{
-        num_digit.push(user_resp);
-        quant_resp+= 1;
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#nova_tarefa input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
 
-        if(user_resp < resposta){
-            msg1.textContent = "*Dica: tente um número mais alto";
-            msg2.textContent = "Quantidade de respostas: " + quant_resp;
-            msg3.textContent = "Números digitados: " +
-            num_digit;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
         }
-        else if(user_resp > resposta){
-            msg1.textContent = "*Dica: tente um número mais baixo";
-            msg2.textContent = "Quantidade de respostas: " + quant_resp;
-            msg3.textContent = "Números digitados: " +
-            num_digit;
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
         }
-        else if(user_resp == resposta){
-            msg1.textContent = "Parabéns, você acertou!";
-            msg2.textContent = "O número correto é: " + resposta;
-            msg3.textContent = "Você adivinhou em "+ quant_resp + " tentativas";
-            document.getElementById("my_btn").disabled = true;
-        }
+
+        document.querySelector("#nova_tarefa input").value = "";
     }
 }
